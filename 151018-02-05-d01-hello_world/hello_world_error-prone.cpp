fa5b3c73f7c16c280ae5b000a2b578714214989f
R4mzy <github@r4mzy.co.za>
2015-10-20 22:26:43 +0200

    setup for test02
    intentional misspelling of std_lib_facilities.h

--- 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
+++ 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
@@ -1,7 +1,9 @@
 // this source meant to test the behaviour of g++ compiler when there are (syntactical) errors in the source code
 
 // test01 - commenting #include line to pretend it was left out...
-// #include "std_lib_facilities.h"
+// test02 - include but intentionally misspell std_lib_facilities.h
+
+#include "stdlib_facilities.h"
 
 int main() {
 	cout << "Hello, World!\n";
