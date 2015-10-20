abd811bfbb35ad52a082638a536d19f628f66e9c
R4mzy <github@r4mzy.co.za>
2015-10-20 22:11:29 +0200

    created hello_world_error-prone.cpp and testing first copilation with intentional error
    error tested is commenting out '#include "std_lib_facilities.h"' and compiling

---
+++ 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
@@ -0,0 +1,10 @@
+// this source meant to test the behaviour of g++ compiler when there are (syntactical) errors in the source code
+
+// test01 - commenting #include line to pretend it was left out...
+// #include "std_lib_facilities.h"
+
+int main() {
+	cout << "Hello, World!\n";
+	keep_window_open();
+	return 0;
+}
