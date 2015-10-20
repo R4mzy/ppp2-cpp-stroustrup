803c10c39797140fe59d8992759e9233dc9e6b73
R4mzy <github@r4mzy.co.za>
2015-10-20 22:33:28 +0200

    setup for test03
    "forgetting" to terminate a string with "

--- 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
+++ 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
@@ -2,11 +2,11 @@
 
 // test01 - commenting #include line to pretend it was left out...
 // test02 - include but intentionally misspell std_lib_facilities.h
-
+// test03 - "forget" to terminate string on line 9 with a "
 #include "stdlib_facilities.h"
 
 int main() {
-	cout << "Hello, World!\n";
+	cout << "Hello, World!\n;
 	keep_window_open();
 	return 0;
 }
