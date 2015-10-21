b5cf07a58fa144c9dc759dcbe679e62658aa18d0
R4mzy <github@r4mzy.co.za>
2015-10-21 21:17:33 +0200

    setup for test04 - calling integer main() rather than int main()

--- 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
+++ 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
@@ -3,9 +3,11 @@
 // test01 - commenting #include line to pretend it was left out...
 // test02 - include but intentionally misspell std_lib_facilities.h
 // test03 - "forget" to terminate string on line 9 with a "
+// test04 - call main() function using "integer" instead on "int" (this has to do with the expected result of the function)
+
 #include "std_lib_facilities.h"
 
-int main() {
+integer main() {
 	cout << "Hello, World!\n;
 	keep_window_open();
 	return 0;
