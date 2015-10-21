288c63f55054c8b20a472e38ae23f2516bf7d6be
R4mzy <github@r4mzy.co.za>
2015-10-21 21:29:52 +0200

    resetup of test05
    i mucked up my comments and, well, forgot to comment them...

--- 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
+++ 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
@@ -5,7 +5,7 @@
 // test03 - "forget" to terminate string on line 9 with a "
 // test04 - call main() function using "integer" instead on "int" (this has to do with the expected result of the function)
 // test05 - use only one < for directing the "input" for cout - one might expect this to make cout want to "replace" the console? at least if
-		writing a file that might be the case (ie. < is replace where << would be append)
+//		writing a file that might be the case (ie. < is replace where << would be append)
 
 #include "std_lib_facilities.h"
 
