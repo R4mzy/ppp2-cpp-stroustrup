e6044baf466362d14d61cd138b144c1e073fff9c
R4mzy <github@r4mzy.co.za>
2015-10-21 21:28:03 +0200

    setup for test05
    using "cout <" rather than "cout <<"

--- 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
+++ 151018-02-05-d01-hello_world/hello_world_error-prone.cpp
@@ -4,11 +4,13 @@
 // test02 - include but intentionally misspell std_lib_facilities.h
 // test03 - "forget" to terminate string on line 9 with a "
 // test04 - call main() function using "integer" instead on "int" (this has to do with the expected result of the function)
+// test05 - use only one < for directing the "input" for cout - one might expect this to make cout want to "replace" the console? at least if
+		writing a file that might be the case (ie. < is replace where << would be append)
 
 #include "std_lib_facilities.h"
 
-integer main() {
-	cout << "Hello, World!\n";
+int main() {
+	cout < "Hello, World!\n";
 	keep_window_open();
 	return 0;
 }
