cfaa8088efa3861231195af2c01224c5c83154dc
R4mzy <github@r4mzy.co.za>
2015-10-27 23:11:20 +0200

    started on source for upstairs_bathroom exercise

---
+++ 151027-ch02-03-e01-02-upstairs_bathroom/upstairs_bathroom.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+// this application is meant to output instructions to the console to explain, in tedious detail, how to get to the upstairs bathroom.
+// it will first give a brief description of the house being navigated
+
+int main() {
+// first describe the house
+cout <<	"Starting from your seated position at the dining room table, you are facing across the table and down a passage. \nFive meters down the passage, are stairs to the left, then a landing and the stairs double back around and continue upward. \nAt the top of the stairs, is a door two meters to the right, which is the bathroom door. \n";
+
+// painfully detailed instructions next
+
+
+// keep the window open...
+keep_window_open();
+
+// return a successfull exit
+return 0;
+}
