38ad784386dadf93202b8b7a0f4ff128a617b08d
R4mzy <github@r4mzy.co.za>
2016-01-14 21:36:32 +0200

    completed c03.09 - further safe and unsafe conversion practice

--- 160114-c03.09.02-unsafe_type_conversion/unsafe_type_experimentation.cpp
+++ 160114-c03.09.02-unsafe_type_conversion/unsafe_type_experimentation.cpp
@@ -12,7 +12,23 @@ int main() {
 			<< " i2==" << i2					// int value of char
 			<< " char(" << c << ")\n";			// the char
 	}
+	// crashes/exits on entry of an invalid double value, like a letter or symbol
+	// surprisingly, some high numbers work (like 45645)
+	
+	/*
+	// can't use this 'cause it'll hit compile errors ;)
+	cout << "\n\nand again but with {}-list notation... \n";
+	while (cin>>d) {						// repeat the statements below
+											// as long as we type in numbers
+		int i {d};							// try to squeeze a double into an int -- compiler returns double to int narrowing conversion error
+		char c {i};							// try to squeeze and int into a char -- compiler returns int to char narrowing conversion
+		int i2 {c};							// get the integer value of the character
+		cout << "d==" << d						// the original double
+			<< " i=="<< i 						// converted to int	
+			<< " i2==" << i2					// int value of char
+			<< " char(" << c << ")\n";			// the char
+	}
+	*/
 }
 
-// crashes on entry of an invalid double value, like a letter or symbol
-// surprisingly, some high numbers work (like 45645)
+// I suppose further reading will tell me why {}-list notation (universal and uniform initialisation) is not just used all the time to eliminate unsafe conversions
