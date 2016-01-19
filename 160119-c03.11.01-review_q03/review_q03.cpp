bb8907e952b4c5b61c23ccfd4bffbc17a152f92e
R4mzy <github@r4mzy.co.za>
2016-01-19 22:03:16 +0200

    completed c03.11 review questions - added some test programs to confirm answers

---
+++ 160119-c03.11.01-review_q03/review_q03.cpp
@@ -0,0 +1,7 @@
+#include "../res-files/std_lib_facilities.h"
+
+int main() {
+	int number;
+	cout << "Please enter an integer number.\n";
+	cin >> number;
+}
---
+++ 160119-c03.11.02-review_q17/review_q17.cpp
@@ -0,0 +1,7 @@
+#include "../res-files/std_lib_facilities.h"
+
+int main() {
+	string a = "alpha";
+	string b = "bravo";
+	cout << a + b;
+}
