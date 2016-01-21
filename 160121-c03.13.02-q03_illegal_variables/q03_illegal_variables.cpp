04c5e048dccb233bed5b151b491a4dd89666d1bc
R4mzy <github@r4mzy.co.za>
2016-01-21 19:27:03 +0200

    completed c03.13 exercise q03 - testing and dumb variable names

---
+++ 160121-c03.13.02-q03_illegal_variables/q03_illegal_variables.cpp
@@ -0,0 +1,14 @@
+#include "../res-files/std_lib_facilities.h"
+
+// testing illegal and stupid-but-legal variable names to see the behaviour of the compiler
+
+int main() {
+	string _underscore = "what does";
+	int ALLCAP = 1919;
+	double if = 4.2;
+	char one char = 'a';
+	string #tag = "hashy";
+	int and!123 = 123;
+	double 2digi = 2.1;
+	char string = 's';
+}
