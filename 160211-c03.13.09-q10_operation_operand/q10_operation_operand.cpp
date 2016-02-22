f3383112d360b6502a85cdcc8722bf398bccd378
R4mzy <github@r4mzy.co.za>
2016-02-22 22:20:39 +0200

    minor edit for legibility

--- 160211-c03.13.09-q10_operation_operand/q10_operation_operand.cpp
+++ 160211-c03.13.09-q10_operation_operand/q10_operation_operand.cpp
@@ -5,7 +5,8 @@
 		+ 100 3.14
 		* 4 5
 
-	Read the operation into a string called operation and use an if-statement to figure out which operation the user wants, for example, if (operation=="+"). Read the operands 
+	Read the operation into a string called operation and use an if-statement to figure out which operation the user wants, for example, if
+	(operation=="+"). Read the operands 
 	into variables of type double. Implement this for operations called +, –, *, /, plus, minus, mul, and div with their obvious meanings.
 */
 
