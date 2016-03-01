295292c4e2033ba89c2a9974c71f742889185142
R4mzy <github@r4mzy.co.za>
2016-03-01 17:00:19 +0200

    started work on c03 exercise q11 - coinage

--- 160301-c03.13.10-q11_coinage/q11_coinage.cpp
+++ 160301-c03.13.10-q11_coinage/q11_coinage.cpp
@@ -15,7 +15,10 @@
 
 	Make some improvements: if only one of a coin is reported, make the output grammatically correct, e.g., 14 dimes and 1 dime (not 1 dimes). Also, report the sum in 
 	dollars and cents, i.e., $5.73 instead of 573 cents.
-
+	
+	part two:
+	Make some improvements: if only one of a coin is reported, make the output grammatically correct, e.g., 14 dimes and 1 dime (not 1 dimes). Also, report the sum in 
+	dollars and cents, i.e., $5.73 instead of 573 cents.
 */
 
 
@@ -23,38 +26,68 @@ int main() {
 	// ask and store how many of each coin the user has
 	cout << "\nHey dude, wanna get takeout? How much money you got on you?\n";
 	// pennies (1-cent)
-	int pennies;
+	double pennies;
 	cout << "\nHow many pennies you got?\n";
 	cin >> pennies;
+	string pennies_grammar = pennies + " pennies";
+	if (pennies == 1) {
+		pennies_grammar = "1 penny";
+	}
+	
 	// nickels (5-cent)
-	int nickels;
+	double nickels;
 	cout << "\nAnd how many nickels?\n";
 	cin >> nickels;
+	string nickels_grammar = nickels + " nickels";
+	if (nickels == 1) {
+		nickels_grammar = "1 nickel";
+	}
+	
 	// dimes (10-cent)
-	int dimes;
+	double dimes;
 	cout << "\nYeah and dimes?\n";
 	cin >> dimes;
+	string dimes_grammar = dimes + " dimes";
+	if (dimes == 1) {
+		dimes_grammar = "1 dime";
+	}
+	
 	// quarters (25-cent)
-	int quarters;
+	double quarters;
 	cout << "\nGive me the one-fourths, the quarters.\n";
 	cin >> quarters;
+	string quarters_grammar = quarters + " quarters";
+	if (quarters == 1) {
+		quarters_grammar = "1 quarter";
+	}
+	
 	// half dollars (50-cent)
-	int half_dollars;
+	double half_dollars;
 	cout << "\nWill you meet me half-dollar way?\n";
 	cin >> half_dollars;
+	string half_dollars_grammar = half_dollars + " half-dollars";
+	if (half_dollars == 1) {
+		half_dollars_grammar = "1 half-dollar";
+	}
+	
 	// one-dollars (100-cent)
-	int one_dollars;
+	double one_dollars;
 	cout << "\nLast and largest, one-dollars?\n";
 	cin >> one_dollars;
+	string one_dollars_grammar = one_dollars + " dollars";
+	if (one_dollars == 1) {
+		one_dollars_grammar = "1 one-dollar";
+	}
 	
 	// print out the users coins
-	cout << "\n\nOkay so we got, uhh, " << pennies << " pennies.\n"
-		<< "And then " << nickels << " nickels and " << dimes << " dimes.\n"
-		<< "Plus, like " << quarters << " quarters.\n"
-		<< "Then, let's see, " << half_dollars << " half-dollars.\n"
-		<< "Lastly, " << one_dollars << " one-dollars.\n";
+	cout << "\n\nOkay so we got, uhh, " << pennies_grammar << ".\n"
+		<< "And then " << nickels_grammar << " and " << dimes_grammar << ".\n"
+		<< "Plus, like " << quarters_grammar << ".\n"
+		<< "Then, let's see, " << half_dollars_grammar << ".\n"
+		<< "Lastly, " << one_dollars_grammar << ".\n";
 	
 	// print out the total cents balance
-	int total_cents = (pennies) + (nickels * 5) + (dimes * 10) + (quarters * 25) + (half_dollars * 50) + (one_dollars * 100);
-	cout << "\n So that gives us a total of " << total_cents << " cents. Sweet.";
+	double total_cents = (pennies) + (nickels * 5) + (dimes * 10) + (quarters * 25) + (half_dollars * 50) + (one_dollars * 100);
+	double total_dollars = total_cents / 100;
+	cout << "\n So that gives us a total of " << total_cents << " cents. Better known as $" << total_dollars << ". Sweet.";
 }
